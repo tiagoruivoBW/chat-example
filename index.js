ad531ba0cceb996e88a6b80e3e8fb339495c6c2f
Carlos del Moral <cdelmoral@bluewolf.com>
2018-04-24 15:41:08 -0500

    no message

--- index.js
+++ index.js
@@ -25,27 +25,27 @@ app.get('/account/:id', function (request, response) {
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
-    io.emit('chat message', msg[1]);
+    io.emit('chat message', msg);
     
-    pg.connect(process.env.DATABASE_URL, function(err, client, done) { 
-      console.log(msg[0]);
-      client.query('SELECT * FROM salesforce.Account WHERE SFID = $1', [msg[0]], function(err, result) {
-        console.log(msg[1]);
-        console.log(msg[0]);
-        console.log(result.rows[0].description);
-        done();
-        if (err)
-        { console.error(err); response.send("Error " + err); }
-        else
-        { 
-          console.log('update');
-          var allMessage = result.rows[0].description + msg[1];
-          client.query('UPDATE salesforce.Account SET Description = $1 WHERE SFID = $2', [allMessage, msg[0]], function(err, result) {
-            done();
-          });
-        }
-      });
-    });
+    // pg.connect(process.env.DATABASE_URL, function(err, client, done) { 
+    //   console.log(msg[0]);
+    //   client.query('SELECT * FROM salesforce.Account WHERE SFID = $1', [msg[0]], function(err, result) {
+    //     console.log(msg[1]);
+    //     console.log(msg[0]);
+    //     console.log(result.rows[0].description);
+    //     done();
+    //     if (err)
+    //     { console.error(err); response.send("Error " + err); }
+    //     else
+    //     { 
+    //       console.log('update');
+    //       var allMessage = result.rows[0].description + msg[1];
+    //       client.query('UPDATE salesforce.Account SET Description = $1 WHERE SFID = $2', [allMessage, msg[0]], function(err, result) {
+    //         done();
+    //       });
+    //     }
+    //   });
+    // });
   });
 });
 
