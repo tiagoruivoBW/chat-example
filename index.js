7af4f9093ae29bbe216be7ee50230fe3cf9b0b81
Carlos del Moral <cdelmoral@bluewolf.com>
2018-04-24 15:20:57 -0500

    no message

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ io.on('connection', function(socket){
     
     pg.connect(process.env.DATABASE_URL, function(err, client, done) { 
       client.query('SELECT * FROM salesforce.Account WHERE SFID = $1', [msg[0]], function(err, result) {
-        // done();
+        done();
         if (err)
         { console.error(err); response.send("Error " + err); }
         else
