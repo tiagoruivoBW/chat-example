076d70ff7617178865344c68bebd7e66015e69ed
Carlos del Moral <cdelmoral@bluewolf.com>
2018-04-24 14:15:56 -0500

    Add view and route

--- index.js
+++ index.js
@@ -2,9 +2,25 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
+var pg = require('pg');
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
+});
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/account/:id', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) { 
+    client.query('SELECT * FROM salesforce.Account WHERE Id = ' + request.params.id, function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/account', {results: result.rows} ); }
+    });
+  });
 });
 
 io.on('connection', function(socket){
